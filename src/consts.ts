8286347626739b1c291b06b2a0866a3ae0be8830
Jeff Helman <jeffhelman42@gmail.com>
2025-04-23 21:43:22 -0700

    Initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '42 Days of AI';
+export const SITE_OWNER = 'Two Agentic Dudes.';
+export const SITE_DESCRIPTION = 'Welcome to you AI adventure!';
