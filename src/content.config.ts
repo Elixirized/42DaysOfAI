cd68f4504987894e4367a7237c284758fa979fe1
Jeff Helman <jeffhelman42@gmail.com>
2025-05-05 21:15:19 -0700

    Sidebar config/formatting/includes

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,12 +7,21 @@ const days = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
+		// allow string _or_ string-array
+    description: z.union([z.string(), z.array(z.string())]),
 		// Transform string to Date object
 		pubDate: z.coerce.date().optional(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 	}),
+	// add a “computed” to always cast it to an array
+	// JSH: Doe NOT work ...
+  computed: {
+    descriptionArray: ({ data }) =>
+      typeof data.description === 'string'
+        ? data.description.split('\n').filter((l) => l.trim().startsWith('-')).map((l) => l.replace(/^- */, '').trim())
+        : data.description,
+  },
 });
 
 export const collections = { days };
